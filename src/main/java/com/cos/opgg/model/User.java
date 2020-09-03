bf19ca6555328bc9d873ac5e887935318d922be8
jaybon1 <ponggas2@gmail.com>
2020-09-03 19:39:04 +0900

    ...

--- src/main/java/com/cos/opgg/model/User.java
+++ src/main/java/com/cos/opgg/model/User.java
@@ -1,21 +1,14 @@
 package com.cos.opgg.model;
 
 import java.sql.Timestamp;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
 
 import org.hibernate.annotations.CreationTimestamp;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
